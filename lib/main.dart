a82a358873f988ff1b3e0c1eac93a896ec144060
wsimple1028 <wjh1994101@yahoo.co.jp>
2021-06-10 22:08:30 +0900

    lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -18,20 +18,46 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ContentWidget extends StatelessWidget {
+class ContentWidget extends StatefulWidget {
+  ContentWidget() {
+    print("call back 1");
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    print("call back 7");
+    return ContentWidgetState();
+  }
+}
+
+class ContentWidgetState extends State<ContentWidget> {
+  ContentWidgetState() {
+    print ("call back 2");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    print("call back 3");
+  }
+
+  @override
+  void didChangeDependencies() {
+    print("call back 4");
+  }
+
+  @override
+  void didUpadateWidget() {
+    print("call back 5");
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text(
-        "hello world",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-            fontSize: 20,
-            backgroundColor: Color.fromARGB(120, 120, 120, 120)
-        ),
-      ),
+      child: Text("hello world", style: TextStyle(fontSize: 30),),
     );
   }
 }
 
-class
+
+
